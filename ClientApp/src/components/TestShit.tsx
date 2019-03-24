e1b310e35e972e297b403ddb7045c846014fba3f
Jerry Backer <jwb726@gmail.com>
2019-03-24 12:17:39 -0500

    Added css transitions

--- ClientApp/src/components/TestShit.tsx
+++ ClientApp/src/components/TestShit.tsx
@@ -1,23 +1,64 @@
 import React, { Component } from "react";
-import styled from "styled-components";
+import { OrangeDiv, MyButton, RemoveButton } from "./styledComponents";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import "./test.scss";
 
 type TestShitProps = {};
 
-export class TestShit extends Component<TestShitProps> {
+type TestShitState = {
+  items: string[];
+  counter: number;
+};
+
+export class TestShit extends Component<TestShitProps, TestShitState> {
+  constructor(props: TestShitProps) {
+    super(props);
+
+    this.state = {
+      items: ["One", "Two", "Foure"],
+      counter: 1
+    };
+  }
+
   render() {
+    const items = this.state.items.map((item, index) => {
+      return (
+        <div key={item} onClick={() => this.removeItem(item)}>
+          <div key={item}>{item}</div>
+        </div>
+      );
+    });
 
-    const OrangeDiv = styled.div`
-        padding: 0 10px;
-        background-color: orange;
-        transition: color 1s 1.5s, background-color 2s, border-radius 2s;
+    return (
+      <div>
+        <ReactCSSTransitionGroup
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={600}
+          transitionAppear={true}
+          transitionAppearTimeout={500}
+          transitionName="spicy"
+        >
+          {items}
+        </ReactCSSTransitionGroup>
+        <MyButton onClick={() => this.addItem("foobar")}>Add Item</MyButton>
+        <OrangeDiv>Test Shit</OrangeDiv>
+      </div>
+    );
+  }
 
-        :hover {
-            background-color: blue;
-            border-radius: 10px;
-            color: white;
-        }
-    `;
+  removeItem(item: string) {
+    const newItems = this.state.items.filter(i => i !== item);
+    this.setState(Object.assign({}, this.state, { items: newItems }));
+  }
 
-    return <OrangeDiv>Test Shit</OrangeDiv>;
+  addItem(item: string) {
+    const realItem = item + " " + this.state.counter;
+    const newItems = [...this.state.items, realItem];
+    this.setState(
+      Object.assign({}, this.state, {
+        items: newItems,
+        counter: this.state.counter + 1
+      })
+    );
   }
 }
