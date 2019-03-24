5b86e5b24037372e56c690ea5c035e22682caa21
Jerry Backer <jwb726@gmail.com>
2019-03-24 10:31:25 -0500

    Added simple css transition

--- ClientApp/src/components/TestShit.tsx
+++ ClientApp/src/components/TestShit.tsx
@@ -7,7 +7,15 @@ export class TestShit extends Component<TestShitProps> {
   render() {
 
     const OrangeDiv = styled.div`
+        padding: 0 10px;
         background-color: orange;
+        transition: color 1s 1.5s, background-color 2s, border-radius 2s;
+
+        :hover {
+            background-color: blue;
+            border-radius: 10px;
+            color: white;
+        }
     `;
 
     return <OrangeDiv>Test Shit</OrangeDiv>;
